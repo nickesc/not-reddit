b15f75a8302ed9a46e53f770c6523dfe8069be60
N. Escobar <nick@nickesc.io>
2026-02-22 18:57:38 -0500

    add new card styles

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -63,7 +63,7 @@ function PostList({
             <ul className="post-list-body">
                 {childPosts.map((p) => {
                     return (
-                        <li className="content-card" key={p.id}>
+                        <li className={`card content-card ${p.author === userId && "identity-self-card"}`} key={p.id}>
                             <PostBubble
                                 post={p}
                                 userId={userId}
